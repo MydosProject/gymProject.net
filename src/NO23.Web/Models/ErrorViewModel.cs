524d3bb365948adf0185c57dc7ebe116d55245bc
pinaryilmaz <haticepinary@icloud.com>
2026-07-13 15:46:52 +0300

    chore: initialize NO23 Sports Club project

---
+++ src/NO23.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NO23.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ tests/NO23.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace NO23.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
